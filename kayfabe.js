b2293b133751e04b2e91cecafe468d49d723f2c1
byronicone <byron.e.barnes@gmail.com>
2018-06-28 16:39:01 -0500

    Added sinon mocking to tests

--- kayfabe.js
+++ kayfabe.js
@@ -1,5 +1,3 @@
-var kayfabe = {};
-
 const Sentiment = require('sentiment');
 const dotenv = require('dotenv');
 const colors = require('colors/safe');
@@ -23,55 +21,6 @@ const config_twitter = {
 };
 
 let api = new Twit(config_twitter);
-
-async function getWrestlerSentiments(wrestlers) {
-  for(let wrestler of wrestlers){
-    let sentiment = new Sentiment();
-    let count = 100;
-    let tweets = await getTweets(wrestler, count);
-    let pos = 0;
-    let neg = 0;
-    for (tweet of tweets) {
-        let score = sentiment.analyze(tweet).comparative;
-        tweet = `${tweet}\n`;
-        if (score > 0) {
-          pos++;
-        } else if (score < 0) {
-          neg++;
-        }
-    }
-
-    let message = `${wrestler}: ${pos} positive vs. ${neg} negative.\n`;
-    console.log(message.color(pos-neg));
-  }
-}
-
-kayfabe.getPageOfTweets = async function getPageOfTweets(q, count){
-    try{
-      let tweets = await api.get('search/tweets', {q, count, tweet_mode: 'extended', include_entities: false});
-      return tweets.data.statuses.reduce((uniqueTweets, oneTweet) => {
-        if(oneTweet.retweeted_status == null){
-          uniqueTweets.push(R.pick(['full_text'], oneTweet));
-        }
-        return uniqueTweets;
-      }, []);
-    }
-    catch(e){
-      console.log('Could not get page of tweets');
-      return e;
-    }
-}
-
-kayfabe.getTweets = async function getTweets(q, count) {
-    try{
-      let tweets = await kayfabe.getPageOFTweets(q, count);
-    }
-    catch(err){
-      console.log(`This query triggered an error:  ${q}`);
-      console.log(err);
-    }
-}
-
 String.prototype.color = function(sentiment){
   if(sentiment > 5){
     return colors.green(this);
@@ -84,8 +33,54 @@ String.prototype.color = function(sentiment){
   }
 }
 
-for(prop in kayfabe) {
-   if(kayfabe.hasOwnProperty(prop)) {
-     module.exports[prop] = kayfabe[prop];
-   }
+module.exports = {
+  getSentiment: async function(wrestlers) {
+    let result = {positive:[], negative:[]};
+
+    for(let wrestler of wrestlers){
+      let sentiment = new Sentiment();
+      let count = 100;
+      let tweets = await this.getTweets(wrestler, count);
+      let overallSentiment = 0;
+      for (tweet of tweets) {
+          let score = sentiment.analyze(tweet.full_text).comparative;
+          if (score > 0) {
+            result.positive.push(tweet);
+          } else if (score < 0) {
+            result.negative.push(tweet);
+          }
+      }
+    }
+
+    return result;
+  },
+
+  getPageOfTweets: function(fn, endpoint, options){
+    return async function(){
+        let tweets = await fn.call(endpoint, options);
+        return tweets.data.statuses.reduce((uniqueTweets, oneTweet) => {
+          if(oneTweet.retweeted_status == null){
+            uniqueTweets.push(R.pick(['full_text'], oneTweet));
+          }
+          return uniqueTweets;
+        }, []);
+    }
+  },
+
+  getTweets: async function(q, count) {
+      try{
+        let page = 1;
+        let tweets = [];
+        let getPage = this.getPageOfTweets(api.get, 'search/tweets', {q, count, tweet_mode: 'extended', include_entities: false})
+        while(page){
+          tweets.concat(await getPage());
+          page = tweets.page;
+        }
+        return tweets;
+      }
+      catch(err){
+        console.log(`This query triggered an error:  ${q}`);
+        console.log(err);
+      }
+  }
 }
--- test/twitter_test.js
+++ test/twitter_test.js
@@ -1,18 +1,33 @@
+var assert = require('chai').assert;
 var expect = require('chai').expect;
+var sinon = require('sinon');
 var kf = require('../kayfabe');
 var fs = require('fs');
 
+const page = require('./fixtures/page_1.json');
+const testTweets = require('./fixtures/two_tweets.json');
+const opts = {q:'Roman Reigns', count:5, tweet_mode: 'extended', include_entities: false};
+
 describe('sentiment analysis of wrestling-relating tweets', () =>{
-  before( () => {
-  })
   it('gets one page of tweets about Roman Reigns', async () => {
-    let tweets = await kf.getPageOfTweets('Roman Reigns', 100);
-    expect(tweets).to.have.length;
+    let callback = sinon.fake.returns(page);
+    let getPage = kf.getPageOfTweets(callback, 'search/tweets', opts);
+    let tweets =  await getPage();
+    assert(callback.calledOnce,'API was not called exactly once.');
+    assert(callback.calledOn('search/tweets'), 'Wrong twitter endpoint was called');
+    assert(callback.calledWith(opts), 'Options invalid for node_twitter call');
+    expect(tweets.length).to.equal(2);
     tweets.map( (tweet) => {
       expect(tweet).to.be.an('object')
         .that.includes.all.keys('full_text');
     })
   })
-  it('gets the text from a non-duplicate tweet by a valid user', () => {
+  it('analyzes the sentiment of tweets', async () => {
+      let stub = sinon.stub(kf, "getTweets");
+      stub.returns(testTweets);
+      let sentiments = await kf.getSentiment(['RomanReigns']);
+      assert(stub.calledOnce, 'getTweets was not called exactly once');
+      expect(sentiments.positive.length).to.equal(1);
+      expect(sentiments.negative.length).to.equal(1);
   })
 })
